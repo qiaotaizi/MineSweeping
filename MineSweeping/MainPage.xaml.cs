9059200c56df807e40d29c4d8070ba26d8bed6d0
zhiheng.jiang <zhiheng.jiang@ttpai.cn>
2019-03-08 18:57:21 +0800

    雷池的初始化逻辑

--- MineSweeping/MainPage.xaml.cs
+++ MineSweeping/MainPage.xaml.cs
@@ -28,31 +28,165 @@ namespace MineSweeping
     public sealed partial class MainPage : Page
     {
 
-        private static int _blockWidth=50;
+        private static int _blockWidth = 50;
 
+        //雷池长宽 同时也是地雷数量
         public static int PoolSize = 10;
+
+        private static Random _mineGenerator = new Random();
 
         public ObservableCollection<MineBlock> MineBlocks;
 
         public GridLength GridLength = new GridLength(PoolSize * _blockWidth);
 
+        private int[,] _mineArr;
+
         public MainPage()
         {
             this.InitializeComponent();
+            LoadData();
+        }
 
+        private void LoadData()
+        {
+            RefreshMineArr();
             MineBlocks = new ObservableCollection<MineBlock>();
-            for(int i = 0; i < 100; i++)
+            for (int i = 0; i < 100; i++)
             {
-                MineBlocks.Add(new MineBlock() { CordX = 1, CordY = 1, isMine = true, MinesAround = 1 });
+                MineBlock mine = new MineBlock();
+                InitMineBlock(mine, i);
+                MineBlocks.Add(mine);
             }
-            
+        }
 
+        private void ReloadData()
+        {
+            RefreshMineArr();
+            foreach (MineBlock block in MineBlocks)
+            {
+                ReinitMineBlock(block);
+            }
+        }
+
+        private void ReinitMineBlock(MineBlock mine)
+        {
+            var isMine = false;
+            for (int j = 0; j < PoolSize; j++)
+            {
+                if (mine.CordX == _mineArr[j, 0] && mine.CordY == _mineArr[j, 1])
+                {
+                    isMine = true;
+                    break;
+                }
+            }
+            mine.IsMine = isMine;
+            if (isMine)
+            {
+                mine.MinesAround = 0;
+            }
+            else
+            {
+                var ma = 0;
+                //计算周围数量
+                for (var x = -1; x < 2; x++)
+                {
+                    for (var y = -1; y < 2; y++)
+                    {
+                        if (CordInMineArr(x, y))
+                        {
+                            ma++;
+                        }
+                    }
+                }
+                mine.MinesAround = ma;
+            }
+        }
+
+        //初始化块信息
+        private void InitMineBlock(MineBlock mine, int i)
+        {
+            mine.CordX = i % PoolSize;
+            mine.CordY = i / PoolSize;
+            var isMine = false;
+            for(int j = 0; j < PoolSize; j++)
+            {
+                if(mine.CordX==_mineArr[j,0] && mine.CordY == _mineArr[j, 1])
+                {
+                    isMine = true;
+                    break;
+                }
+            }
+            mine.IsMine = isMine;
+            if (isMine)
+            {
+                mine.MinesAround = 0;
+                mine.Color = MineBlock.IS_MINE;
+            }
+            else
+            {
+                var ma = 0;
+                //计算周围数量
+                for (var x=-1;x<2;x++)
+                {
+                    for (var y=-1;y<2;y++)
+                    {
+                        if (CordInMineArr(mine.CordX+x, mine.CordY+y))
+                        {
+                            ma++;
+                        }
+                    }
+                }
+                mine.MinesAround = ma;
+                mine.Color = MineBlock.IS_NOT_MINE;
+            }
+            Debug.WriteLine("Mine="+mine.CordX+","+mine.CordY+","+mine.IsMine+","+mine.MinesAround);
+        }
+
+        private bool CordInMineArr(int x, int y)
+        {
+            for(var i = 0; i < PoolSize; i++)
+            {
+                if (_mineArr[i,0]==x && _mineArr[i,1]==y)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //刷新/重置数组
+        private void RefreshMineArr()
+        {
+            _mineArr = new int[PoolSize, 2];
+            for (int i = 0; i < PoolSize;)
+            {
+                var randX = _mineGenerator.Next(0,PoolSize-1);
+                var randY = _mineGenerator.Next(0,PoolSize-1);
+                bool foundSameCord = false;
+                //向前遍历
+                for (int j = 0; j < i; j++)
+                {
+                    if(randX==_mineArr[j,0] && randY == _mineArr[j, 1])
+                    {
+                        //生成的两次位置相同,重新随机位置
+                        foundSameCord = true;
+                        break;
+                    }
+                }
+                if (foundSameCord)
+                {
+                    continue;
+                }
+                _mineArr[i, 0] = randX;
+                _mineArr[i, 1] = randY;
+                Debug.WriteLine(i+"索引("+randX+","+randY+")");
+                i++;
+            }
         }
 
         private void ReloadButton_Click(object sender, RoutedEventArgs e)
         {
-            var rootFrame=Window.Current.Content as Frame;
-            rootFrame.Navigate(typeof(MainPage));
+            ReloadData();
         }
     }
 }
---
+++ MineSweeping/Models/MineBlock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace MineSweeping.Models
+{
+    public class MineBlock
+    {
+
+        public static SolidColorBrush IS_NOT_MINE = new SolidColorBrush(Colors.LightGray);
+        public static SolidColorBrush IS_MINE = new SolidColorBrush(Colors.Red);
+
+        public int CordX { get; set; }
+        public int CordY { get; set; }
+        public bool IsMine { get; set; }
+        public int MinesAround { get; set; }
+        public Brush Color { get; set; }
+    }
+}
